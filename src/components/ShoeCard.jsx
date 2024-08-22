f12f5f1f6f9d5ebea5fe5f1dcdde698bcc3c2ab7
Ahmad Raza Gul <ahmadrg91@gmail.com>
2024-08-22 06:21:58 +0500

    minor UI fixes

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -13,7 +13,7 @@ const ShoeCard = ({ shoe, currentShoe, changeCurrentShoe }) => {
                 "border-transparent"} 
             cursor-pointer max-sm:flex-1 rounded-xl border-2`} 
         onClick={handleClick}>
-        <div className="flex justify-center items-center bg-card bg-cover bg-center sm:h-40 sm:w-40 max-sm:p-4">
+        <div className="flex justify-center rounded-xl items-center bg-card bg-cover bg-center sm:h-40 sm:w-40 max-sm:p-4">
             <img
                 src={shoe.thumbnail}
                 alt="shoes collection"
