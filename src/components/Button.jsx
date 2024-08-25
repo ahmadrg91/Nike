194f7a1c6575bd4486dd17f0644aaa6233e1baa5
Ahmad Raza Gul <ahmadrg91@gmail.com>
2024-08-25 20:54:15 +0500

    added Special Offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ label, icon}) => {
+const Button = ({ label, icon, bg, text, border}) => {
   return (
-    <button className='flex gap-4 items-center justify-center bg-coral-red px-7 py-4 rounded-full text-white text-lg leading-none font-montserrat border-corel-red'>
+    <button className={`flex gap-2 items-center justify-center px-7 py-4 rounded-full text-lg leading-none font-montserrat border ${bg || 'bg-coral-red'} ${text || 'text-white'} ${border || 'border-corel-red'}`}>
         {label}
         {icon && <img src={icon}/>}
     </button>
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,7 +1,26 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-between items-center gap-10 max-container max-xl:flex-col-reverse">
+      <div className="flex-1 flex justify-center items-center">
+        <img src={offer} width={773} height={687} className="object-contain w-full"/>
+      </div>
+      <div className="flex flex-col flex-1">
+        <h2 className="text-4xl font-palanquin font-bold lg:max-w-lg"><span className="text-coral-red">Special</span> Offer</h2>
+        <div className="font-montserrat info-text">
+          <p className="mt-4">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.</p>
+          <p className="mt-6">Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.</p>
+        </div>
+        <div className="flex flex-wrap gap-4 mt-11">
+          <Button label="View Details" icon={arrowRight}/>
+          <Button label="Learn more" bg="bg-white" text="text-slate-gray" border="border-slate-gray"/>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default SpecialOffer
+export default SpecialOffer;
