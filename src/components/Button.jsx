c070195fba64ff1c723602c22cc5652db99123f2
Ahmad Raza Gul <ahmadrg91@gmail.com>
2024-08-26 14:38:36 +0500

    added Subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ label, icon, bg, text, border}) => {
+const Button = ({ label, icon, bg, text, border, fullWidth}) => {
   return (
-    <button className={`flex gap-2 items-center justify-center px-7 py-4 rounded-full text-lg leading-none font-montserrat border ${bg || 'bg-coral-red'} ${text || 'text-white'} ${border || 'border-corel-red'}`}>
+    <button className={`flex gap-2 items-center justify-center px-7 py-4 rounded-full text-lg leading-none font-montserrat border ${fullWidth && 'w-full'} ${bg || 'bg-coral-red'} ${text || 'text-white'} ${border || 'border-corel-red'}`}>
         {label}
         {icon && <img src={icon}/>}
     </button>
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,19 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="flex max-lg:flex-col max-container items-center justify-between gap-10">
+      <h2 className="text-4xl font-bold font-palanquin leading-[68px] lg:max-w-md">
+        Sign Up for <span className="text-coral-red">Updates</span> &amp; Newsletter
+      </h2>
+      <div className="w-full lg:max-w-[40%] items-center flex max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="email" placeholder="subscribe@nike.com" className="input"/>
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth/>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Subscribe
+export default Subscribe;
