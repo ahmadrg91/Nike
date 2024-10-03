ca28fc6ca53152b87c52095d03ce34c0907f66f1
Ahmad Raza Gul <133594951+ahmadrg91@users.noreply.github.com>
2024-10-03 19:16:36 +0500

    Update CustomerReviews.jsx

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -4,7 +4,7 @@ import ReviewCard from "../components/ReviewCard";
 const CustomerReviews = () => {
   return (
     <section className="max-container">
-      <h2 className="text-4xl font-bold font-palanquin m-auto text-center text-white">What Our <span className="text-coral-red">Customers</span> Say?</h2>
+      <h2 className="text-4xl font-bold font-palanquin m-auto text-center dark:text-white">What Our <span className="text-coral-red">Customers</span> Say?</h2>
       <p className="info-text lg:max-w-lg mt-6 m-auto text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
       <div className="flex items-center justify-evenly max-xl:flex-col mt-24 gap-14">
         { reviews.map(review => <ReviewCard key={review.customerName} name={review.customerName} rating={review.rating} feedback={review.feedback} imgURL={review.imgURL}/>) }
